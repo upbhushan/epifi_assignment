c1e8743c7ff2a604b7d33115f3a5ebe1aa58f94a
Bhushan127 <uprikarbhushan300@gmail.com>
2025-07-25 15:17:41 +0530

    models

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true }
+});
+
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
